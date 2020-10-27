17b6a197977a483d72b859370039c7425127e722
sachinumrao <troublemaker1512@gmail.com>
2020-10-28 00:34:09 +0530

    minor changes

--- LM/entity_extractor.py
+++ LM/entity_extractor.py
@@ -1,6 +1,8 @@
 import spacy
 import pickle
 import pandas as pd
+import pathlib
+from tqdm import tqdm
 
 class EntityExtractor:
     """
@@ -10,8 +12,7 @@ class EntityExtractor:
     def __init__(self, input_file, output_file):
         self.input_file = input_file
         self.output_file = output_file
-        self.entity_file = None
-        self.fact_triplets = None
+        self.fact_triplet_df = None
         
         self.sentences = []
         self.raw_triplets = []
@@ -22,18 +23,32 @@ class EntityExtractor:
         with open(self.input_file, 'rb') as f:
             self.sentences = pickle.load(f)
         
-    def check_entity_disambiguation(self, entity):
+    def _check_entity_disambiguation(self, entity):
         pass
         
         
     def find_entities(self, sent):
         pass
     
-    def format_triplets(self):
-        self.fact_triplets = pd.DataFrame(columns=['Head', 'Relation', 'Tail'])
+    def save_triplets(self):
+        self.fact_triplet_df = pd.DataFrame(columns=['Head', 'Relation', 'Tail'])
+        
+        # self.fact_triplet_df.to_csv(self.output_file, index=False)
         pass
     
-    def save_entity_list(self):
-        with open(self.entity_file, 'w') as f:
+    def save_entity_list(self, entity_file_name):
+        with open(entity_file_name, 'w') as f:
             for item in self.entity_list:
                 f.write("%s\n" % item)
+                
+                
+if __name__ == "__main__":
+    corpus_file =  pathlib.Path.home().joinpath('Data', 'LM_Data', 'harry_sentences.pkl')
+    output_file = pathlib.Path.home().joinpath('Data', 'LM_Data', 'fact_triplets.csv')
+    entity_file = pathlib.Path.home().joinpath('Data', 'LM_Data', 'harry_potter_entities.txt')
+    entity_finder = EntityExtractor(corpus_file, 
+                                    output_file)
+    
+    entity_finder.load_sentence_corpus()
+    
+    pass
